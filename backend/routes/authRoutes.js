72e4f31faa60802d8d4d644f67169eef7bbbde35
AiOfLautech <babalolahephzibah2@gmail.com>
2025-03-27 14:54:20 +0100

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,13 +5,13 @@ const authController = require('../controllers/authController');
 // Public user signup
 router.post('/signup', authController.registerUser);
 
-// Public login (for both users and admins)
+// Public login route (for both public users and admins)
 router.post('/login', authController.loginUser);
 
-// Reset password using username
+// Reset password route
 router.post('/reset-password', authController.resetPassword);
 
-// Admin signup (protected by invite key)
+// Admin signup route (protected by invite key)
 router.post('/admin-signup', authController.registerAdmin);
 
 module.exports = router;
