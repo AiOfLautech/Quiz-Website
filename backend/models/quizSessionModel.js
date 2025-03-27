21379a940188aec0727d4433b8b9e39e3ddbc842
AiOfLautech <babalolahephzibah2@gmail.com>
2025-03-27 08:03:10 +0100

    Update quizSessionModel.js

--- backend/models/quizSessionModel.js
+++ backend/models/quizSessionModel.js
@@ -1,1 +1,39 @@
 // quizSessionModel.js placeholder
+const mongoose = require('mongoose');
+
+const quizSessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    courseId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true,
+    },
+    // Array of question objects (shuffled for this session)
+    questions: {
+      type: Array,
+      required: true,
+    },
+    // User responses, corresponding to the questions order
+    answers: {
+      type: Array,
+      default: [],
+    },
+    startTime: {
+      type: Date,
+      default: Date.now,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('QuizSession', quizSessionSchema);
