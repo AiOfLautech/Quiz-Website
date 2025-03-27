bce354cc9f493e096e169ad907df7515bcf66ded
AiOfLautech <babalolahephzibah2@gmail.com>
2025-03-27 12:41:47 +0100

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,9 +3,11 @@ const User = require('../models/userModel');
 
 module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization;
+
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
   }
+
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -13,10 +15,8 @@ module.exports = async (req, res, next) => {
     if (!user) {
       return res.status(401).json({ message: "User not found" });
     }
-    if (user.role !== 'admin') {
-      return res.status(403).json({ message: "Access forbidden: Admins only" });
-    }
-    req.user = user; // Set the user for further use in controllers
+
+    req.user = user;
     next();
   } catch (error) {
     console.error("Authentication error:", error);
