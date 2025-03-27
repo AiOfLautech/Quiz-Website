1039bc0b5406fb8fd1b92537e7da693019b6db59
AiOfLautech <babalolahephzibah2@gmail.com>
2025-03-27 10:39:02 +0100

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Announcement Endpoints
 router.post('/announcement', /* authMiddleware, */ adminController.createAnnouncement);
