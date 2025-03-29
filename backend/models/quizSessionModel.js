8bb06470c8be0041a54816e5170c289cc48cb9b3
AiOfLautech <babalolahephzibah2@gmail.com>
2025-03-29 08:58:12 +0100

    Update quizSessionModel.js

--- backend/models/quizSessionModel.js
+++ backend/models/quizSessionModel.js
@@ -1,39 +1,33 @@
-// quizSessionModel.js placeholder
 const mongoose = require('mongoose');
 
-const quizSessionSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    courseId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Course',
-      required: true,
-    },
-    // Array of question objects (shuffled for this session)
-    questions: {
-      type: Array,
-      required: true,
-    },
-    // User responses, corresponding to the questions order
-    answers: {
-      type: Array,
-      default: [],
-    },
-    startTime: {
-      type: Date,
-      default: Date.now,
-      required: true,
-    },
-    endTime: {
-      type: Date,
-      default: null,
-    },
+const quizSessionSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
   },
-  { timestamps: true }
-);
+  courseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Course',
+    required: true,
+  },
+  questions: {
+    type: Array,
+    required: true,
+  },
+  answers: {
+    type: Array,
+    default: [],
+  },
+  startTime: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  endTime: {
+    type: Date,
+    default: null,
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model('QuizSession', quizSessionSchema);
