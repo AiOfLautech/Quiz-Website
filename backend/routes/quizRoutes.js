f561da78c22e577165f3015e1ed256594c0abf91
AiOfLautech <babalolahephzibah2@gmail.com>
2025-03-30 07:21:28 +0100

    Update quizRoutes.js

--- backend/routes/quizRoutes.js
+++ backend/routes/quizRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const quizController = require('../controllers/quizController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+// Protected endpoints: user must be logged in to start and submit quizzes
 router.post('/start', authMiddleware, quizController.startQuiz);
 router.post('/submit', authMiddleware, quizController.submitQuiz);
 
