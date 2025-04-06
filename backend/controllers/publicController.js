aa2b86650437413d6f599156dc250642b6196a0e
AiOfLautech <babalolahephzibah2@gmail.com>
2025-04-06 14:31:32 +0100

    Update publicController.js

--- backend/controllers/publicController.js
+++ backend/controllers/publicController.js
@@ -3,7 +3,6 @@ const Course = require('../models/courseModel');
 const Accommodation = require('../models/accommodationModel');
 const Discussion = require('../models/discussionModel');
 
-// Get all announcements (public)
 exports.getAnnouncements = async (req, res) => {
   try {
     const announcements = await Announcement.find().sort({ createdAt: -1 });
@@ -14,7 +13,6 @@ exports.getAnnouncements = async (req, res) => {
   }
 };
 
-// Get all courses (public)
 exports.getCourses = async (req, res) => {
   try {
     const courses = await Course.find();
@@ -25,7 +23,6 @@ exports.getCourses = async (req, res) => {
   }
 };
 
-// Get all accommodations (public)
 exports.getAccommodations = async (req, res) => {
   try {
     const accommodations = await Accommodation.find().sort({ createdAt: -1 });
@@ -36,12 +33,9 @@ exports.getAccommodations = async (req, res) => {
   }
 };
 
-// Get all discussion posts (public)
 exports.getDiscussions = async (req, res) => {
   try {
-    const discussions = await Discussion.find()
-      .populate('createdBy', 'username')
-      .sort({ createdAt: -1 });
+    const discussions = await Discussion.find().populate('createdBy', 'username').sort({ createdAt: -1 });
     res.json({ discussions });
   } catch (error) {
     console.error("Error retrieving discussions:", error);
