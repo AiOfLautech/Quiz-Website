c59949bb01b1f32a4f0916ea411c9ac2e7d1bc1c
AiOfLautech <babalolahephzibah2@gmail.com>
2025-04-08 21:33:22 +0100

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Admin-protected endpoints
+// Announcement routes
 router.post('/announcement', authMiddleware, adminController.createAnnouncement);
 router.patch('/announcement/:announcementId', authMiddleware, adminController.updateAnnouncement);
 router.delete('/announcement/:announcementId', authMiddleware, adminController.deleteAnnouncement);
 
+// Accommodation routes
 router.post('/accommodation', authMiddleware, adminController.createAccommodation);
 router.patch('/accommodation/:accommodationId', authMiddleware, adminController.updateAccommodation);
 router.delete('/accommodation/:accommodationId', authMiddleware, adminController.deleteAccommodation);
