3a1a3a01214b556d4094f3460080d53aaf72c75f
AiOfLautech <babalolahephzibah2@gmail.com>
2025-04-09 07:55:44 +0100

    Update and rename announcementModel.js to Announcement.js

---
+++ backend/models/Announcement.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const announcementSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Announcement title is required"],
+    trim: true,
+  },
+  content: {
+    type: String,
+    required: [true, "Content is required"],
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Announcement", announcementSchema);
